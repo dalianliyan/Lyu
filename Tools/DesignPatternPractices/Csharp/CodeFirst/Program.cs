4c5e528110b9c4a0a9b38913c94b218e565f2a1b
Lee.Yan <dalianliyan@gmail.com>
2026-01-07 18:17:05 +0800

    add project: design pattern practices

---
+++ Tools/DesignPatternPractices/Csharp/CodeFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Users u = new Users() { ID = 0 };            
+            using (DBContextCodeFirst db = new DBContextCodeFirst())
+            {
+                db.Users.Add(u);
+                db.SaveChanges();
+            }
+        }
+    }
+}
